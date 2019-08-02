50b09120ee9c43a59115b8f533f24002ac6be001
Lionel <lionellorenzomartin@gmail.com>
2019-08-02 11:33:15 +0200

    Repositorio: Busqueda de ofertas vigentes, creada

--- reto-final-master/PetclincBack/src/main/java/org/springframework/samples/petclinic/repository/OfertasRepository.java
+++ reto-final-master/PetclincBack/src/main/java/org/springframework/samples/petclinic/repository/OfertasRepository.java
@@ -2,7 +2,17 @@ package org.springframework.samples.petclinic.repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.samples.petclinic.model.Oferta;
+
 @Repository
 public interface OfertasRepository extends JpaRepository<Oferta, Integer> {
+	
+	@Query("select o from ofertas o where to_char(:expireDate,'DD/MM/YYYY')>to_char(SYSDATE,'DD/MM/YYYY')")
+    List<Oferta> findByExpireDate(@Param("expireDate")String expireDate);
 
 }
